4f40ed72e5c5f6fd3cb9ade04ca723f4798c2ca9
edobasky <edore7@gmail.com>
2024-12-13 13:04:20 +0100

    Add project files.

---
+++ BEffectWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BEffectWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
