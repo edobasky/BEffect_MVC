75292e084c053751267bdc4b694da7de83016473
edobasky <edore7@gmail.com>
2024-12-14 08:34:44 +0100

    Code base setup

---
+++ BEffectWeb/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using BEffectWeb.Data;
+using BEffectWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BEffectWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _appDb;
+
+        public CategoryController(AppDbContext appDb)
+        {
+            _appDb = appDb;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Category> objCategoryList = await _appDb.Categories.ToListAsync();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
