b83f0fe520faa75bbc9887c6a1371aaa5dd7188a
edobasky <edore7@gmail.com>
2024-12-21 19:42:51 +0100

    view model added

--- BEffectWeb.DataAccess/Migrations/20241217103815_imageUrl.Designer.cs
+++ BEffectWeb.DataAccess/Migrations/20241217103815_imageUrl.Designer.cs
@@ -3,6 +3,7 @@ using BEffectWeb.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BEffectWeb.DataAccess.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241217103815_imageUrl")]
+    partial class imageUrl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BEffectWeb/Areas/Admin/Controllers/ProductController .cs
@@ -0,0 +1,120 @@
+﻿using BEffectWeb.DataAccess.Repository.Interface;
+using BEffectWeb.Models;
+using BEffectWeb.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BEffectWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public List<SelectListItem> selectLists = new();
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Product> objCategoryList = await _unitOfWork.Product.GetAll();
+
+            return View(objCategoryList);
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetDropDown()
+        {
+            IEnumerable<SelectListItem> CategoryList = (await _unitOfWork.Category.GetAll()).Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString(),
+            });
+
+         //   selectLists = CategoryList.ToList();
+            return CategoryList.ToList();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var catList =  await GetDropDown();
+
+            ProductVM productVM = new ProductVM()
+            {
+                CategoryList = catList,
+                Product = new Product()
+            };
+            return View(productVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductVM obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj.Product);
+                await _unitOfWork.SaveAsync();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                obj.CategoryList = await GetDropDown();
+                return View(obj);
+            }
+           
+
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id is null || id == 0) return NotFound();
+
+            Product? productFromDb = await _unitOfWork.Product.GetByCondition(u => u.Id == id);
+            if (productFromDb == null) return NotFound();
+
+            return View(productFromDb);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                await _unitOfWork.SaveAsync();
+                TempData["success"] = "Product updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id is null || id == 0) return NotFound();
+
+            Product? productFromDb = await _unitOfWork.Product.GetByCondition(u => u.Id == id);
+            if (productFromDb == null) return NotFound();
+
+            return View(productFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeletePOST(int? id)
+        {
+            Product? obj = await _unitOfWork.Product.GetByCondition(u => u.Id == id);
+            if (obj == null) return NotFound();
+
+            _unitOfWork.Product.Remove(obj);
+            await _unitOfWork.SaveAsync();
+            TempData["success"] = "pRODUCT deleted successfully";
+            return RedirectToAction("Index");
+
+
+        }
+    }
+}
