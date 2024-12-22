9ef5cad7cc0aae26fe7c7d99aef7cd59b314de46
edobasky <edore7@gmail.com>
2024-12-22 20:00:12 +0100

    added scafolding identity

--- BEffectWeb.DataAccess/Data/AppDbContext.cs
+++ BEffectWeb.DataAccess/Data/AppDbContext.cs
@@ -1,15 +1,19 @@
 ﻿using BEffectWeb.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BEffectWeb.DataAccess.Data
 {
-    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext(options)
     {
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                  new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
--- BEffectWeb/Program.cs
+++ BEffectWeb/Program.cs
@@ -3,12 +3,16 @@ using BEffectWeb.DataAccess.Data;
 using BEffectWeb.DataAccess.Repository;
 using BEffectWeb.DataAccess.Repository.Interface;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
 
 
@@ -26,9 +30,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
